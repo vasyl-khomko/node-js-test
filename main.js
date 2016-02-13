3659e2b2c6fa039752cb1a6fa1942b0adfae18b6
Vasyl Khomko <Vasyl Khomko>
2016-02-13 11:21:47 +0200

    Change main.js for using on OpenShift server.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var http = require("http");
 
+var serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8081;
+var serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 http.createServer(function (request, response) {
 
    // Send the HTTP header
@@ -9,7 +12,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(8081);
+}).listen(serverPort, serverIpAddress);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log("Server running at " + serverIpAddress + ":" + serverPort);
